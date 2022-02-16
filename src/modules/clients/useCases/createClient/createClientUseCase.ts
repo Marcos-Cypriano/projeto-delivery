0be49d17ca8bb7cc9c2c80b69dc197c9246e0492
Marcos-Cypriano <ma_cypriano@hotmail.com>
2022-02-16 10:29:19 -0300

    Corrected error in creating user and deliveryman, added new routes

--- src/modules/clients/useCases/createClient/createClientUseCase.ts
+++ src/modules/clients/useCases/createClient/createClientUseCase.ts
@@ -14,6 +14,7 @@ export class CreateClientUseCase {
         const clientExist = await prisma.clients.findFirst({
             where: {
                 username: {
+                    equals: username,
                     mode: "insensitive"
                 }
             }
--- src/routes.ts
+++ src/routes.ts
@@ -7,25 +7,39 @@ import { CreateDeliveryController } from "./modules/deliveries/UseCases/createDe
 import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient"
 import { FindAllAvailableController } from "./modules/deliveries/UseCases/findAllAvailable/findAllAvailableController"
 import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateDeliveryman"
+import { UpdateDeliverymanController } from "./modules/deliveries/UseCases/updateDeliveryman/UpdateDeliverymanController"
+import { FindAllDeliveriesController } from "./modules/clients/useCases/deliveries/findAllDeliveriesController"
 
 const routes = Router()
 
-const authenticateClientController = new AuthenticateClientController()
-const authenticateDeliverymanController = new AuthenticateDeliverymanController()
-
+//Client
 const createClientController = new CreateClientController()
+const authenticateClientController = new AuthenticateClientController()
+const findAllDeliveriesClient = new FindAllDeliveriesController()
+
+//Deliveryman
 const createDeliverymanController = new CreateDeliverymanController()
+const authenticateDeliverymanController = new AuthenticateDeliverymanController()
+const updateDeliverymanController = new UpdateDeliverymanController()
+
+//Delivery
 const createDeliveryController = new CreateDeliveryController()
+const findAllAvailableController = new FindAllAvailableController()
 
-const findAllAvailableController = new FindAllAvailableController
 
+//Client
+routes.post('/client/', createClientController.handle)
 routes.post('/client/authenticate', authenticateClientController.handle)
+routes.get('/client/deliveries', ensureAuthenticateClient, findAllDeliveriesClient.handle)
+
+//Deliveryman
+routes.post('/deliveryman/', createDeliverymanController.handle)
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
 
-routes.post('/client/', createClientController.handle)
-routes.post('/deliveryman/', createDeliverymanController.handle)
+//Delivery
 routes.post('/delivery/', ensureAuthenticateClient, createDeliveryController.handle)
-
 routes.get('/delivery/available', ensureAuthenticateDeliveryman, findAllAvailableController.handle)
+routes.put('/delivery/updateDeliveryman/:id', ensureAuthenticateDeliveryman, updateDeliverymanController.handle)
+
 
 export {routes}
